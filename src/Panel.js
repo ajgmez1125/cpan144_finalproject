df4335eec8c1cd5195a9e6c7ecdcbe32e36136ea
Ayden Gomez <ajgg1125@gmail.com>
2023-04-15 20:46:49 -0400

    Added multi day forecast and altered UI still needs work, just a general idea fr the ui. ill leave the rest fr u alex

--- src/Panel.js
+++ src/Panel.js
@@ -4,18 +4,49 @@ function Panel(props)
 {
     const[weatherType, setWeatherType] = useState();
     const[description, setDescription] = useState();
+    //const[weatherImg, setWeatherImg] = useState();
+
     const[temperature, setTemperature] = useState();
     const[feelsLike, setFeelsLike] = useState();
+
+    const[humidity, setHumidity] = useState()
+    const[windSpeed, setWindSpeed] = useState()
+    
     const[time, setTime] = useState();
     const deg = '\u00B0'
 
     useEffect(() => {
         setTime(props.forecast.dt_txt)
+
         setWeatherType(props.forecast.weather[0].main)
         setDescription(props.forecast.weather[0].description)
+        // setWeatherImg();
+
         setTemperature(adjustTemp(props.forecast.main.temp))
         setFeelsLike(adjustTemp(props.forecast.main.feels_like));
+
+        setHumidity(props.forecast.main.humidity)
+        setWindSpeed(props.forecast.wind.speed)
     }, [props.unit])
+
+    // const setWeatherImg = () =>
+    // {
+    //     switch(weatherType)
+    //     {
+    //         case "cloudy":
+    //             break;
+    //         case "rainy":
+    //             break;
+    //         case "clear":
+    //             break;
+    //         case "windy":
+    //             break;
+    //         case "sunny":
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    // }
 
     const adjustTemp = (temp) =>
     {
@@ -36,12 +67,16 @@ function Panel(props)
     }
 
     return(
-        <div>
+        <div class = 'weather_panel'>
             <p>{time}</p>
             <h2>{weatherType}</h2>
             <p>{description}</p>
-            <h4>Temperature is {temperature + deg}{props.unit}</h4>
-            <h4>Feels like {feelsLike + deg}{props.unit}</h4>
+            <div class = 'temperature_subpanel'>
+                <h4>Temperature is {temperature + deg}{props.unit}</h4>
+                <h4>Feels like {feelsLike + deg}{props.unit}</h4>
+                <h4>Humidity: {humidity}%</h4>
+                <h4>Wind speed: {windSpeed}mp/h</h4>
+            </div>
         </div>
     )
 }
--- src/Weather.js
+++ src/Weather.js
@@ -11,20 +11,34 @@ function Weather(props)
     useEffect(() => {
         setCity(props.forecast.city.name)
         setCountry(props.forecast.city.country)
-    }, [])
+    }, [country, city])
 
     const returnPanel = () => {
         if(props.forecast !== undefined)
         {
-            return <Panel forecast = {props.forecast.list[0]} unit = {unit}/>
+            return (
+                returnEachNthElement(8, props.forecast.list).map((data) => {
+                    return <Panel forecast = {data} unit = {unit}/>
+                })
+            )
         }
+    }
+
+    const returnEachNthElement = (n, arr) => {
+        var newArr = []
+        for(var i = 0; i < arr.length; i = i + n)
+        {
+            newArr.push(arr[i])
+        }
+        return newArr
     }
 
     return(
         <div>
             <h1>{city} {country}</h1>
+            <TemperatureSwitch switcher = {setUnit}/>
             {
-                returnPanel(0)
+                returnPanel()
             }
             <TemperatureSwitch switcher = {setUnit}/>
         </div>
