8d4b2574ba4907e2316f94080c9a9ab1f1588d2d
Alexandr <programalexandr@gmail.com>
2023-04-18 00:22:04 -0400

    no periodTS

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ function Home()
         <div className='homediv'>
             <h1>Weather Connection</h1>
             <h3>Please Input City or Country</h3>
-            <h3>You wish to see the weather for.</h3>
+            <h3>You wish to see the weather for</h3>
             <Search />
         </div>
     )
